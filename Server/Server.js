0af492e54a85d13800760fdd4873ed510293b4b5
paupc <p.pulido.c@institutvidreres.cat>
2023-03-29 09:55:44 +0200

    Examen_UF1_AD_PauPulido

--- Server/Server.js
+++ Server/Server.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 const fs2 = require('node:fs');
 const path = require('node:path');
 const readdirp = require("readdirp");
+const StreamConcat = require('stream-concat');
 
 
 app.use(cors());
@@ -15,10 +16,10 @@ app.listen(port, () => {
   console.log(`Server listening on the port:: ${port}`)
 })
 
+//EX1
 app.post('/api/ex1', (req,res) => {
 
   const arrelInici = 'C:\\Users\\paupc\\Desktop\\DAM\\AD\\UF1_Expaupulido\\UF1_ExamenAaD';
-
   console.log('Les rutes de tot el que inclueix el directori UF1_ExamenAaD');
   readdirp(arrelInici,{type: 'all'})
     .on('data', (ruta) => {
@@ -27,13 +28,29 @@ app.post('/api/ex1', (req,res) => {
 
 })
 
+//EX2
 app.post('/api/ex2',(req,res) => {
-  const readStream = fs.createReadStream('C:\\Users\\paupc\\Desktop\\DAM\\AD\\UF1_Expaupulido\\UF1_ExamenAaD\\Documents\\FitxerOrigen.txt')
-  const writeStream = fs.createWriteStream('C:\\Users\\paupc\\Desktop\\DAM\\AD\\UF1_Expaupulido\\UF1_ExamenAaD\\Documents\\Docs1\\FitxerDesti.txt')
-  readStream.pipe(writeStream)
-  console.log("Els dos fitxers tenen el mateix contingut")
+
+  if(fs.existsSync('C:\\Users\\paupc\\Desktop\\DAM\\AD\\UF1_Expaupulido\\UF1_ExamenAaD\\Documents\\Docs1\\FitxerDesti.txt')){
+    //Concatena (Aquest funciona pero concatena més d'una vegada no se perque)
+    const stream1 = fs.createReadStream('C:\\Users\\paupc\\Desktop\\DAM\\AD\\UF1_Expaupulido\\UF1_ExamenAaD\\Documents\\FitxerOrigen.txt', 'utf8');
+    const stream2 = fs.createReadStream('C:\\Users\\paupc\\Desktop\\DAM\\AD\\UF1_Expaupulido\\UF1_ExamenAaD\\Documents\\Docs1\\FitxerDesti.txt', 'utf8');
+    const output = fs.createWriteStream('C:\\Users\\paupc\\Desktop\\DAM\\AD\\UF1_Expaupulido\\UF1_ExamenAaD\\Documents\\Docs1\\FitxerDesti.txt', 'utf8');
+    const combinedStream = new StreamConcat([stream1, stream2]);
+    combinedStream.pipe(output);
+    console.log("Concatenat");}
+
+  else {
+    //Crea el nou fitxer i copia les dades del fitxer Origen. (Awuest va be)
+      const readStream = fs.createReadStream('C:\\Users\\paupc\\Desktop\\DAM\\AD\\UF1_Expaupulido\\UF1_ExamenAaD\\Documents\\FitxerOrigen.txt')
+      const writeStream = fs.createWriteStream('C:\\Users\\paupc\\Desktop\\DAM\\AD\\UF1_Expaupulido\\UF1_ExamenAaD\\Documents\\Docs1\\FitxerDesti.txt')
+      readStream.pipe(writeStream)
+      console.log("Els dos fitxers tenen el mateix contingut")
+    }
+
 })
 
+//EX3
 app.post('/api/ex3', (req,res) => {
   fs.readdir('C:\\Users\\paupc\\Desktop\\DAM\\AD\\UF1_Expaupulido\\UF1_ExamenAaD\\Imatges', (err, imatge) => {
     imatge.forEach(imatge => {
@@ -41,11 +58,9 @@ app.post('/api/ex3', (req,res) => {
         console.log(imatge);
     })
   })
-
-
-
 })
 
+//EX4
 app.post('/api/ex4', (req,res) => {
 
   fs.readdir('C:\\Users\\paupc\\Desktop\\DAM\\AD\\UF1_Expaupulido\\UF1_ExamenAaD\\Imatges', (err, imatge) => {
